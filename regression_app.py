5b23652b674c057daf4ad205f774d6bfbe11eaf7
SURYA1804 <suryaprakashm246@gmail.com>
2025-08-02 13:07:02 +0530

    Integrate the regression

--- regression_app.py
+++ regression_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pickle
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('regression_model.h5')
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
@@ -19,19 +19,21 @@ with open('scaler.pkl', 'rb') as file:
 
 
 ## streamlit app
-st.title('Customer Churn PRediction')
+st.title('Customer Salary Prediction')
 
+print(ohe_encoder_geo.categories_[0])
 # User input
 geography = st.selectbox('Geography', ohe_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+exited = st.selectbox('Exited', [0, 1])
 tenure = st.slider('Tenure', 0, 10)
 num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
+
 
 # Prepare the input data
 input_data = pd.DataFrame({
@@ -43,8 +45,10 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
+print('feature_names_in_ ', scaler.feature_names_in_)
+print('input_data ', input_data.columns)
 
 # One-hot encode 'Geography'
 geo_encoded = ohe_encoder_geo.transform([[geography]]).toarray()
@@ -57,13 +61,9 @@ input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis
 input_data_scaled = scaler.transform(input_data)
 
 
-# Predict churn
+# Predict Salary
 prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+print("prediction ", prediction)
+prediction_salary = prediction[0][0]
 
-st.write(f'Churn Probability: {prediction_proba:.2f}')
-
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-else:
-    st.write('The customer is not likely to churn.')
+st.write(f'Salary: {prediction_salary:.2f}')
